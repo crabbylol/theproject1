f1a905b1dd10cf88789619d22cd18244c1639fd4
Courtney Lee <courtneylee@courtneys-mbp.lan>
2024-07-12 17:44:46 -0700

    Update

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -1,20 +1,23 @@
 import 'dart:js_interop';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:theproject1/journalentry.dart';
 
 import 'auth_service.dart';
 
 class DatabaseService {
   final _fire = FirebaseFirestore.instance;
-  final _auth = AuthService();
+  final _auth = FirebaseAuth.instance;
 
   create(JournalEntry journalEntry ) {
+    final User? currentUser = _auth.currentUser;
+
     try {
       _fire.collection("journalEntries").add({
         "dateTIme": Timestamp.fromDate(journalEntry.dateTime),
         "content": journalEntry.content,
-        "userID": _auth.getCurrentUser().uid
+        "userID": currentUser.uid
       });
     } catch (e) {
       print(e.toString());
